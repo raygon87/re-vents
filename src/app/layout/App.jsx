ca783296aeb26be428248525f117b18ddac12ea4
Ray Gonzales <raygon8712@gmail.com>
2019-04-01 11:52:59 -0600

    added navbar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello Pogi!</h1>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
