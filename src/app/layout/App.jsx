15e21f8fb19ddfb07f8ba02398362b678849627d
Ray Gonzales <raygon8712@gmail.com>
2019-04-03 09:32:32 -0600

    added routes

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,6 +3,13 @@ import { Container } from 'semantic-ui-react';
 import './App.css';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import { Route } from 'react-router-dom'
+import EventForm from '../../features/event/EventForm/EventForm';
+import HomePage from '../../features/home/HomePage'
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage'
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard'
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage'
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard'
 
 class App extends Component {
   render() {
@@ -10,7 +17,13 @@ class App extends Component {
       <div>
         <NavBar/>
         <Container className="main">
-          <EventDashboard/>
+          <Route path='' exact='true' component={HomePage}/>
+          <Route path='/events' component={EventDashboard}/>
+          <Route path='/events/:id' component={EventDetailedPage}/>
+          <Route path='/people' component={PeopleDashboard}/>
+          <Route path='/profile/:id' component={UserDetailedPage}/>
+          <Route path='/settings' component={SettingsDashboard}/>
+          <Route path='/crateEvent' component={EventForm}/>
         </Container>
       </div>
     );
