855f902ab630e8474f6ea2463c71d188457a58bc
Ray Gonzales <raygon8712@gmail.com>
2019-04-04 00:36:24 -0600

    created event action, reducer and constants

--- src/features/testarea/testActions.jsx
+++ src/features/testarea/testActions.jsx
@@ -10,4 +10,4 @@ export const decrementCounter = () => {
   return {
     type: DECREMENT_COUNTER
   }
-}
+} 
--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -5,24 +5,26 @@ const initialState = {
   data: 42
 }
 
-export const incrementCounter = (state, payload) => {
-  return {...state, data: state.data + 1};
-}
-
-export const decrementCounter = (state, payload) => {
-  return {...state, data: state.data - 1};
-}
-
 // const testReducer = (state = initialState, action) => {
 //   switch (action.type) {
 //     case INCREMENT_COUNTER:
-//     return {...state, data: state.data + 1};
+//       return {...state, data: state.data + 1};
 //     case DECREMENT_COUNTER:
 //       return {...state, data: state.data - 1};
 //     default:
 //       return state;
 //   }
 // }
+
+// export default testReducer
+
+const incrementCounter = (state, payload) => {
+  return {...state, data: state.data + 1};
+}
+
+const decrementCounter = (state, payload) => {
+  return {...state, data: state.data - 1};
+}
 
 export default createReducer(initialState, {
   [INCREMENT_COUNTER]: incrementCounter,
