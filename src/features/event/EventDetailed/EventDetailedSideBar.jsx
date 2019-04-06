2fdd75854bc1b485dead56bb2c836e74570b7e9f
Ray Gonzales <raygon8712@gmail.com>
2019-04-06 11:00:25 -0600

    finished static fetailed page

--- src/features/event/EventDetailed/EventDetailedSideBar.jsx
+++ src/features/event/EventDetailed/EventDetailedSideBar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Segment, List, Label, Item } from 'semantic-ui-react'
 
 const EventDetailedSideBar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+       <div>
+          <Segment
+            textAlign="center"
+            style={{ border: 'none' }}
+            attached="top"
+            secondary
+            inverted
+            color="teal"
+          >
+            2 People Going
+          </Segment>
+          <Segment attached>
+            <List relaxed divided>
+              <Item style={{ position: 'relative' }}>
+                <Label
+                  style={{ position: 'absolute' }}
+                  color="blue"
+                  ribbon="right"
+                >
+                  Host
+                </Label>
+                <Item.Image size="tiny" src="/assets/user.png" />
+                <Item.Content verticalAlign="middle">
+                  <Item.Header as="h3">
+                    <a>Attendee Name</a>
+                  </Item.Header>
+                </Item.Content>
+              </Item>
+            </List>
+          </Segment>
+        </div>
   )
 }
 
