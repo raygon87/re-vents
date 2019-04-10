f56084de82aa19d570c1d93adc657b0608bf1e65
Ray Gonzales <raygon8712@gmail.com>
2019-04-10 13:33:39 -0600

    added redux toastr

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as FormReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr';
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 import modalsReducer from '../../features/modals/modalReducer';
 import authReducer from '../../features/auth/authReducer';
-import asyncReducer from '../../features/async/asyncReducer'
+import asyncReducer from '../../features/async/asyncReducer';
 
 const rootReducer = combineReducers({
   test: testReducer,
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
   form: FormReducer,
   modals: modalsReducer,
   auth: authReducer,
-  async: asyncReducer
+  async: asyncReducer,
+  toastr: toastrReducer
 });
 
 export default rootReducer;
